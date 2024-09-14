efde52f01f6e04897bd4a968dc38b3d38caabf50
kullup <joloschu@gmail.com>
2024-09-14 13:09:22 +0200

    remove emtpy dashboard and redirect to workout index

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [WorkoutController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
